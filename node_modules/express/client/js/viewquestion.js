b29e919faf2e34b3dfc80038cff5fa9ba5db4a4d
bmwatson888 <bmwatson@csu.fullerton.edu>
2015-03-05 14:15:57 -0800

    Further updates to base HTML/JS files
    'Demo-able' version of the site is now available with this version.

--- node_modules/express/client/js/viewquestion.js
+++ node_modules/express/client/js/viewquestion.js
@@ -14,9 +14,9 @@ function showQuestion() {
 function showAnswers() {
 	"use strict";
 	//sample data
-	$(".col-md-11").append("<h2>[3 Votes] Answer 1: Interstellar</h2><h3>Submitted by: vishal on 03/05/2015</h3><p><a class='btn btn-default' href='#' role='button'>Vote for this answer &raquo;</a></p>");
-	$(".col-md-11").append("<h2>[1 Votes] Answer 1: Avengers</h2><h3>Submitted by: vishal on 03/05/2015</h3><p><a class='btn btn-default' href='#' role='button'>Vote for this answer &raquo;</a></p>");
-	$(".col-md-11").append("<h2>[0 Votes] Answer 1: Hot Tub Time Machine 2</h2><h3>Submitted by: brian on 03/04/2015</h3><p><a class='btn btn-default' href='#' role='button'>Vote for this answer &raquo;</a></p>");
+	$(".col-md-11").append("<h2>[3 Votes] Answer 1: Interstellar</h2><h3>Submitted by: vishal on 03/05/2015</h3><p><a class='btn btn-default' href='vote' role='button'>Vote for this answer &raquo;</a></p>");
+	$(".col-md-11").append("<h2>[1 Votes] Answer 1: Avengers</h2><h3>Submitted by: vishal on 03/05/2015</h3><p><a class='btn btn-default' href='vote' role='button'>Vote for this answer &raquo;</a></p>");
+	$(".col-md-11").append("<h2>[0 Votes] Answer 1: Hot Tub Time Machine 2</h2><h3>Submitted by: brian on 03/04/2015</h3><p><a class='btn btn-default' href='vote' role='button'>Vote for this answer &raquo;</a></p>");
 		
 };
 
