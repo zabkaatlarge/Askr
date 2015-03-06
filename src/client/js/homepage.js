1c3e758ad68e77df938fbb46e4b91a2eea71ddb6
bmwatson888 <bmwatson@csu.fullerton.edu>
2015-03-06 09:37:44 -0800

    Removed unneeded dependencies
    Removed some unused dependencies from the HTML files to resolve script
    errors. Changed .htm extensions to .html

--- src/client/js/homepage.js
+++ src/client/js/homepage.js
@@ -6,7 +6,7 @@ showQuestions();
 function showQuestions() {
 	"use strict";
 	//sample data
-	$(".col-md-11").append("<h2>Question: <a href='viewquestion.htm'>What movie should we go to?</a></h2><h3>Submitted by: brian on 03/04/2015</h3><p>3 current possible answers, 8 total votes</p><p><a class='btn btn-default' href='viewquestion.htm' role='button'>View answers &raquo;</a></p>");
+	$(".col-md-11").append("<h2>Question: <a href='viewquestion.html'>What movie should we go to?</a></h2><h3>Submitted by: brian on 03/04/2015</h3><p>3 current possible answers, 8 total votes</p><p><a class='btn btn-default' href='viewquestion.html' role='button'>View answers &raquo;</a></p>");
 		
 };
 
