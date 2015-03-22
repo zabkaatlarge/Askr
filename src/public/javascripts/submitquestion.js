32f7b910669d26aee8582abd0cfba8cc158f7089
Brian Watson <bmwatson@csu.fullerton.edu>
2015-03-21 17:08:08 -0700

    got submit question page to post question to DB

--- src/public/javascripts/submitquestion.js
+++ src/public/javascripts/submitquestion.js
@@ -7,6 +7,6 @@ showSubmitForm();
 function showSubmitForm() {
 	"use strict";
 	//sample data
-	$(".col-md-11").append("<form class='navbar-form'><div class='form-group'><input type='text' placeholder='Enter your question' class='form-control'></div><button type='submit' class='btn btn-success'>Submit</button></form>");
+	$(".col-md-11").append("<form class='navbar-form' action='/submit' method='get'><div class='form-group'><input type='text' placeholder='Enter your question' class='form-control' name='statement'></div><button type='submit' class='btn btn-success'>Submit</button></form>");
 		
 };
--- src/routes/submit.js
+++ src/routes/submit.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var userModel = require('../model/questionModel');
+var questionModel = require('../model/questionModel');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-   var question_id = req.query.question_id;
+   var question_id = 0;
     var statement = req.query.statement;
     var votes = 0;
     var views = 0;
     var comments = "";
 
-    saveOrUpdateQuestion(question_id,statement,votes,views,comments);
+    questionModel.saveOrUpdateQuestion(question_id,statement,votes,views,comments);
 
     res.redirect("/");
 
