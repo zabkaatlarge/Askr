40405f51924c8d5dffcd40c3f2bc0341899a8f09
Brian Watson <bmwatson888@gmail.com>
2015-03-29 13:43:18 -0700

    add login requirement for posting questions and comments

--- src/public/javascripts/login.js
+++ src/public/javascripts/login.js
@@ -4,11 +4,11 @@ loginStatus();
 function loginStatus() {
 	"use strict";
 	//changes header depending on login status
-	
+
 	var url = "http://localhost:3000/checksession";
 	$.getJSON(url, function(data) {
 		if (data[0].status === "in") {
-			$(".login_header").append("<form class='navbar-form navbar-right'><div class='form-group'><input type='text' placeholder='Welcome "+data[0].username+"!' class='form-control'></div></form> - <a href='/logout'>Logout</a>");
+			$(".login_header").append("<form class='navbar-form navbar-right'><div class='form-group'><input type='text' placeholder='Welcome "+data[0].username+"!' class='form-control'></div> - <a href='/logout'>Logout</a></form>");
 		} else {
 			$(".login_header").append("<form class='navbar-form navbar-right' action='/login' method='get'><div class='form-group'><input type='text' placeholder='username' class='form-control' name='username'></div><div class='form-group'><input type='password' placeholder='password' class='form-control' name='password'></div><button type='submit' class='btn btn-success'>Sign in</button></form>");
 		}
